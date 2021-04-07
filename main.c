35d6c7dc4a34557610a930b767e247a1715ebf8b
Hector <hector@purcombe.co.uk>
2021-04-07 14:01:30 +0100

    Add movement

--- main.c
+++ main.c
@@ -7,14 +7,18 @@
 
 char debug_str[80];
 
+// Queue for flood fill
 int flood_fill_queue[QUEUE_MAX][2] = { [ 0 ... (QUEUE_MAX-1) ] = {-1, -1} };
 int queue_front = 0;
 int queue_back = -1;
 int item_count = 0;
 
-// int goal_pos[4][2] = {{7, 5},{7,4},{6,5},{6,4}};
-
+// Constants
 int goal_pos[4][2] = {{3,2},{3,3},{4,2},{4,3}};
+
+// Maze and robot gloabls
+int current_coords[2] = {0,0};
+int current_direction = 0;
 
 int maze_map[8][6] = {
     {5, 1, 1, 1, 1, 8},
@@ -54,13 +58,17 @@ void log(char* text) {
     fflush(stderr);
 }
 
-void discover_walls(int *current_position, int current_direction){
+// ========================================================
+// Wall discovery
+// ========================================================
+
+void discover_walls(){
 	int left = API_wallLeft();
     int right = API_wallRight();
     int front = API_wallFront();
     
-    int x = current_position[0];
-    int y = current_position[1];
+    int x = current_coords[0];
+    int y = current_coords[1];
 
     int wall_values[4] = {0,0,0,0};
 
@@ -110,55 +118,9 @@ void discover_walls(int *current_position, int current_direction){
 	maze_map[x][y] = wall_values[current_direction];
 }
 
-int make_turn(int left_or_right, int current_direction){
-    // If the robot is turing left
-    if (left_or_right == 0){
-
-        // Update the direction indicator without going below 0
-        if(current_direction == 0){
-            current_direction = 3;
-        } else{
-            current_direction = current_direction - 1;
-        }
-
-        // Turn left
-        API_turnLeft();
-    }
-
-    // IF the robot is turing right
-    if (left_or_right == 1){
-        // Update direction indicator without going above 3
-        current_direction = (current_direction + 1) % 4;
-
-        // turn right
-        API_turnRight();
-    }
-
-    // Return the current direction
-    return (current_direction);
-}
-
-void move_forward(int *current_position, int direction){
-    // Move robot forward
-    API_moveForward();
-
-    // Update current coord
-    switch (direction){
-        case(0):
-            current_position[1] += 1;
-            break;
-        case(1):
-            current_position[0] += 1;
-            break;
-        case(2):
-            current_position[1] -= 1;
-            break;
-        case(3):
-            current_position[0] -= 1;
-            break;
-    }
-
-}
+// ========================================================
+// Flood fill
+// ========================================================
 
 int is_accsessable(int x, int y, int direction){
     int wall_layout = maze_map[x][y];
@@ -279,8 +241,8 @@ void update_simulator_map(int x,int y){
 }
 
 void fill_out(x, y){
-    // If the top is accsessable fill in flood value
-    if (is_accsessable(x, y, 0)){
+    // If the top is accsessable from the bttom and the bottom is accsessable from the top fill in flood value
+    if (is_accsessable(x, y, 0) && is_accsessable(x, y+1, 2)){
         if (flood_map[x][y+1] == -1){
             flood_map[x][y+1] = flood_map[x][y] + 1;
             // add to the flood fill queue
@@ -288,8 +250,8 @@ void fill_out(x, y){
         }
     }
 
-    // If the right is accsessable fill in flood value
-    if (is_accsessable(x, y, 1)){
+    // If the right is accsessable from the left and left is accsessable from the right fill in flood value
+    if (is_accsessable(x, y, 1) && is_accsessable(x+1, y, 3)){
         if (flood_map[x+1][y] == -1){
             flood_map[x+1][y] = flood_map[x][y] + 1;
             // add to the flood fill queue
@@ -297,8 +259,8 @@ void fill_out(x, y){
         }
     }
 
-    // If the bottom is accsessable fill in flood value
-    if (is_accsessable(x, y, 2)){
+    // If the bottom is accsessable from the top and the top is accsessable from the bottom fill in flood value
+    if (is_accsessable(x, y, 2) && is_accsessable(x, y-1, 0)){
         if (flood_map[x][y-1] == -1){
             flood_map[x][y-1] = flood_map[x][y] + 1;
             // add to the flood fill queue
@@ -306,8 +268,8 @@ void fill_out(x, y){
         }
     }
 
-    // If the left is accsessable fill in flood value
-    if (is_accsessable(x, y, 3)){
+    // If the left is accsessable from the right and the right is accsessable from the left fill in flood value
+    if (is_accsessable(x, y, 3) && is_accsessable(x-1, y, 1)){
         if (flood_map[x-1][y] == -1){
             flood_map[x-1][y] = flood_map[x][y] + 1;
             // add to the flood fill queue#
@@ -323,7 +285,7 @@ void fill_out(x, y){
 }
 
 void flood_fill(){
-    int current_coords[2];
+    int current_flood_coords[2];
 
     // Reset flood fill map
     memcpy(flood_map, blank_flood_map, sizeof(blank_flood_map));
@@ -336,40 +298,146 @@ void flood_fill(){
         queue_add(goal_pos[i][0], goal_pos[i][1]);
     }
 
-    sprintf(debug_str, "Queue size: %d", item_count);
-    log(debug_str);
-
     // While the queue still contains coords to calculate
     while (!queue_empty()){
-        queue_pop(current_coords);
-        fill_out(current_coords[0], current_coords[1]);
-
-        sprintf(debug_str, "Queue size: %d", item_count);
-        log(debug_str);
+        queue_pop(current_flood_coords);
+        fill_out(current_flood_coords[0], current_flood_coords[1]);
     }
 
 }
 
+// ========================================================
+// Movement
+// ========================================================
+
+void make_turn(int left_or_right){
+    // If the robot is turing left
+    if (left_or_right == 0){
+
+        // Update the direction indicator without going below 0
+        if(current_direction == 0){
+            current_direction = 3;
+        } else{
+            current_direction = current_direction - 1;
+        }
+
+        // Turn left
+        API_turnLeft();
+    }
+
+    // IF the robot is turing right
+    if (left_or_right == 1){
+        // Update direction indicator without going above 3
+        current_direction = (current_direction + 1) % 4;
+
+        // turn right
+        API_turnRight();
+    }
+}
+
+void move_forward(){
+    // Move robot forward
+    API_moveForward();
+
+    // Update current coord
+    switch (current_direction){
+        case(0):
+            current_coords[1] += 1;
+            break;
+        case(1):
+            current_coords[0] += 1;
+            break;
+        case(2):
+            current_coords[1] -= 1;
+            break;
+        case(3):
+            current_coords[0] -= 1;
+            break;
+    }
+
+}
+
+int move_to_next_square(){
+    int x = current_coords[0];
+    int y = current_coords[1];
+
+    // Find the coords around the current coordinate
+    int surrounding_coords[4][2] = {{x,y+1},{x+1,y},{x,y-1},{x-1,y}};
+
+    // Set up varilbes for loop
+    int smallest_flood_map_value = 99;
+    int direction_to_move = -1;
+    int current_flood_map_value = 999;
+
+    // For each direction around the current coord
+    for(int i=0;i<4;i++){
+        // If coordinates are off the maze grid
+        if (surrounding_coords[i][0] > 7 || surrounding_coords[i][0] < 0){
+            continue;
+        }
+        if (surrounding_coords[i][1] > 5 || surrounding_coords[i][1] < 0){
+            continue;
+        }
+        
+        // Check that the surrounding coord is accsessable from current positon
+        if (is_accsessable(x,y,i)){
+            int current_x = surrounding_coords[i][0];
+            int current_y = surrounding_coords[i][1];
+
+            // Get the current value within the coord using the flood map
+            current_flood_map_value = flood_map[current_x][current_y];
+            if (current_flood_map_value < smallest_flood_map_value){
+                // Set the new smallest value around the current coord
+                smallest_flood_map_value = current_flood_map_value;
+
+                // log the direction that we need to move in
+                direction_to_move = i;
+            }
+        }
+    }
+
+    // While we are not facing the correct direction
+    while(current_direction != direction_to_move){
+        // If it takes one turn right to get there turn right
+        if ((current_direction + 1) % 4 == direction_to_move){
+            make_turn(1);
+        }
+        // Else just trun left until we have reached out desired direction
+        else{
+            make_turn(0);
+        }
+    }
+    // Move forward
+    move_forward();
+
+    if(flood_map[current_coords[0]][current_coords[1]] == 0){
+        return(1);
+    } else {
+        return(0);
+    }
+}
 
 int main(int argc, char* argv[]) {
-    log("Running...");
+    int at_goal = 0;
 
-
-    int current_coords[2] = {0,0};
-    int direction = 0;
-    
-    while (1) {
-        discover_walls(current_coords, direction);
-        
+    // WHile not at the finish repeat the main loop
+    while (!at_goal) {
+        discover_walls();
         flood_fill();
+        at_goal = move_to_next_square();
+    }
 
-        if (!API_wallLeft()) {
-            direction = make_turn(0, direction);
-        }
-        while (API_wallFront()) {
-            direction = make_turn(1, direction);
-        }
+    // Reset mouse back to the start
+    API_ackReset();
 
-        move_forward(current_coords, direction);
+    // Reset all relivent mouse varibles while leaving the flood map the same
+    at_goal = 0;
+    current_coords[0] = 0;
+    current_coords[1] = 0;
+    current_direction = 0;
+
+    // Move to the finish
+    while(!at_goal){
+        at_goal = move_to_next_square();
     }
 }
